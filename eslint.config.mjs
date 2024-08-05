a8ffd140488928a8758b6816c46cf11de2b9091a
oliveroliverio <oliveroliverio@gmail.com>
2024-08-05 15:42:25 -0700

    finished linting

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import html from '@html-eslint/eslint-plugin'
+
+export default [
+	// your own configurations.
+	{
+		// recommended configuration included in the plugin
+		...html.configs['flat/recommended'],
+		files: ['**/*.html'],
+		rules: {
+			indent: 'off',
+		},
+	},
+]
